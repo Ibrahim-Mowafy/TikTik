6dc6c96bbd21ff084c502854bdd1d549f6e071a7
Ibrahim Mowafy <ibrahim.mowafy10@gmail.com>
2022-08-03 11:55:58 +0200

    Adding action buttons in video card

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -4,8 +4,19 @@ import { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
-import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
+import LikeButton from './LikeButton';
+import { BASE_URL } from '../utils';
+import axios from 'axios';
+import useAuthStore from '../store/authStore';
+import { FaCheck, FaCommentDots } from 'react-icons/fa';
+import { BiLink } from 'react-icons/bi';
+import { FaLink } from 'react-icons/fa';
+import { useRouter } from 'next/router';
+import { IoMdShareAlt } from 'react-icons/io';
+import { RiShareForwardFill } from 'react-icons/ri';
+import { FcCheckmark } from 'react-icons/fc';
 
 interface IProps {
   post: Video;
@@ -15,7 +26,14 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
   const [isHover, setIsHover] = useState(false);
   const [playing, setPlaying] = useState(false);
   const [isVideoMuted, setIsVideoMuted] = useState(false);
+  const { userProfile }: any = useAuthStore();
+  const [postDetails, setPostDetails] = useState(post);
+  const [likingPost, setLikingPost] = useState(false);
+  const [coping, setCoping] = useState(false);
+
   const videoRef = useRef<HTMLVideoElement>(null);
+  const router = useRouter();
+
   const onVideoPres = () => {
     if (playing) {
       videoRef?.current?.pause();
@@ -27,10 +45,23 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
   };
 
   useEffect(() => {
-    if (videoRef.current) {
+    if (postDetails && videoRef.current) {
       videoRef.current.muted = isVideoMuted;
     }
-  }, [isVideoMuted]);
+  }, [isVideoMuted, postDetails]);
+
+  const handleLike = async (like: boolean) => {
+    if (userProfile) {
+      setLikingPost(true);
+      const res = await axios.put(`${BASE_URL}/api/like`, {
+        userId: userProfile._id,
+        postId: post._id,
+        like,
+      });
+      setPostDetails({ ...postDetails, likes: res.data.likes });
+      setLikingPost(false);
+    }
+  };
 
   return (
     <div className="flex md:gap-5 gap-2 border-b-2 border-gray-200 pb-6">
@@ -51,7 +82,12 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
         </div>
       </div>
 
-      <div className="flex gap-4 relative flex-col pr-2">
+      <div
+        className="flex gap-4 relative flex-col pr-2"
+        //!       issue here in link
+        // onTouchMove={() => setIsHover(true)}
+        // onTouchEnd={() => setIsHover(false)}
+      >
         <div>
           <Link href={`/profile/${post.postedBy._id}`}>
             <div className="flex items-center gap-2 cursor-pointer">
@@ -72,13 +108,12 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
         <div
           onMouseEnter={() => setIsHover(true)}
           onMouseLeave={() => setIsHover(false)}
-          className="rounded-3xl relative"
+          className="rounded-3xl relative flex gap-3"
         >
           <Link href={`/detail/${post._id}`}>
             <video
               ref={videoRef}
               loop
-              // className="lg:max-w-[600px] md:max-w-[400px] max-w-[200px] h-[300px] md:h-[400px] lg:h-[530px] w-full rounded-2xl cursor-pointer bg-black"
               className="lg:max-w-[350px] md:max-w-[250px] max-w-[200px] h-full w-full rounded-2xl cursor-pointer bg-black"
               src={post.video.asset.url}
             ></video>
@@ -108,6 +143,43 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           )}
         </div>
       </div>
+      {userProfile && (
+        <div className="flex flex-col justify-end gap-5 pr-2">
+          <div className={`${likingPost ? 'pointer-events-none' : ''}`}>
+            <LikeButton
+              likes={postDetails.likes}
+              handleLike={() => handleLike(true)}
+              handleDislike={() => handleLike(false)}
+            />
+          </div>
+          <div
+            className="bg-primary rounded-full p-2 md:p-4 cursor-pointer hover:bg-gray-200 transition-colors"
+            onClick={() => {
+              router.push(`/detail/${post._id}`);
+            }}
+          >
+            <FaCommentDots className="text-lg md:text-2xl" />
+          </div>
+          <div className="bg-primary rounded-full p-2 md:p-4 cursor-pointer hover:bg-gray-200 transition-colors">
+            {!coping ? (
+              <BiLink
+                className={`text-lg md:text-2xl transition-transform`}
+                onClick={() => {
+                  setCoping(true);
+                  navigator.clipboard.writeText(
+                    `${BASE_URL}/detail/${post._id}`
+                  );
+                  setTimeout(() => {
+                    setCoping(false);
+                  }, 1000);
+                }}
+              />
+            ) : (
+              <FcCheckmark className={`text-lg md:text-2xl`} />
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
